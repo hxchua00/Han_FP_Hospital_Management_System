2ab90186019a67ae671e60d5be25da075b2d44f3
hxchua00 <hxchua00@gmail.com>
2021-10-27 16:08:52 +0800

    Updated UserManager

--- Han_FP_Hospital_Management_System/UserManager.cs
+++ Han_FP_Hospital_Management_System/UserManager.cs
@@ -12,6 +12,24 @@ namespace Han_FP_Hospital_Management_System
 
         static UserManager()
         {
+            Users AdminAccount = new Users()
+            {
+                Name = "Hospital Admin",
+                ID = 9999,
+                HashedPassword = PasswordCreator.getHashedPassword("Admin123")
+            };
+
+            //Adding Admin account details
+            Users WorkerAccount = new Users()
+            {
+                Name = "Hospital Worker",
+                ID = 1000,
+                HashedPassword = PasswordCreator.getHashedPassword("Worker321")
+            };
+
+            UserAccounts.Add(AdminAccount);
+            UserAccounts.Add(WorkerAccount);
+
             string AddAccountToList = JsonConvert.SerializeObject(UserAccounts);
             File.WriteAllText("Staff_Accounts.Json", AddAccountToList);
 
--- Han_FP_Hospital_Management_System/Users.cs
+++ Han_FP_Hospital_Management_System/Users.cs
@@ -8,28 +8,16 @@ namespace Han_FP_Hospital_Management_System
         public int ID { get; set; }
         public string HashedPassword { get; set; }
 
-        Hospital_Admin AdminAccount;
-        Hospital_Worker WorkerAccount;
-
         public Users()
         {
-            AdminAccount = new Hospital_Admin()
-            {
-                Name = "Hospital Admin",
-                ID = 9999,
-                HashedPassword = PasswordCreator.getHashedPassword("Admin123")
-            };
+            
+        }
 
-            //Adding Admin account details
-            WorkerAccount = new Hospital_Worker()
-            {
-                Name = "Hospital Worker",
-                ID = 1000,
-                HashedPassword = PasswordCreator.getHashedPassword("Worker321")
-            };
-
-            UserManager.UserAccounts.Add(AdminAccount);
-            UserManager.UserAccounts.Add(WorkerAccount);
+        public Users(string Name, int ID, string HashedPassword)
+        {
+            this.Name = Name;
+            this.ID = ID;
+            this.HashedPassword = HashedPassword;
         }
     }
 }
