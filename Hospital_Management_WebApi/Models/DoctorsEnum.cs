7c62d2e0854cfcc9e7122cde6a4fe99b288040a2
hxchua00 <hxchua00@gmail.com>
2021-11-12 03:09:32 +0800

    Refactoring

---
+++ Hospital_Management_WebApi/Models/DoctorsEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Han_FP_Hospital_Management_System
+{
+    public enum DoctorsEnum
+    {
+        Mark,
+        Lee,
+        Kayle,
+        Aisha,
+        Amelia,
+        Reno,
+        Invalid
+    }
+}
---
+++ Hospital_Management_WebApi/Models/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Han_FP_Hospital_Management_System
+{
+    public class Patient
+    {
+        public int PatientID { get; private set; }                  //Unique ID for patients
+        public string PatientName { get; private set; }             //Name of Patient
+        public double PatientWeight { get; private set; }           //Weight of Patient
+        public double PatientHeight { get; private set; }           //Height of Patient
+        public GenderEnum PatientGender { get; private set; }           //Patient's gender
+        public int PatientAge { get; private set; }                 //Age of Patient
+        public EthenicityEnum Ethnicity { get; private set; }               //Race of the Patient
+        public string Address { get; private set; }                 //Patient's Current Staying Address 
+        public int ContactNum { get; private set; }                 //Patient's contact number
+        public List<PatientVisitRecord> VisitHistory { get; private set; }
+
+        public Patient() { }
+
+        public Patient(int totalRegistration, string pName, EthenicityEnum pEthnicity, double pHeight,
+            double pWeight, GenderEnum pGender, string pAddress, int pContactNum, int pAge)
+        {
+            PatientID = 1000 + totalRegistration;
+            PatientName = pName;
+            Ethnicity = pEthnicity;
+            PatientHeight = pHeight;
+            PatientWeight = pWeight;
+            PatientGender = pGender;
+            Address = pAddress;
+            ContactNum = pContactNum;
+            PatientAge = pAge;
+            VisitHistory = new List<PatientVisitRecord>();   
+        }
+
+        public void AddPatientVisitInformation(PatientVisitRecord record)
+        {
+            VisitHistory.Add(record);
+        }
+    }
+       
+}
